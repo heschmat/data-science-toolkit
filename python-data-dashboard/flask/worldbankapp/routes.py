e37adbb0c602e030e73916667d5fb19da9552fa2
Heschmat <heschmat.seyedi@gmail.com>
2021-03-11 15:19:34 +0100

    Add basic functionality for a flask app.

---
+++ python-data-dashboard/flask/worldbankapp/routes.py
@@ -0,0 +1,18 @@
+from worldbankapp import app
+
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+    
+@app.route('/project-one')
+def project_one():
+    return render_template('project_one.html')
+
+@app.route('/flask')
+def project_flask():
+    return render_template('flask.html')
+
+# To tart the web app per, follow the instructions in the instructions.md file.
