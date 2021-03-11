ea6188cbebb78f208e8391e9dc8a22809cb59733
Heschmat <heschmat.seyedi@gmail.com>
2021-03-11 22:42:13 +0100

    Add first plot.

--- python-data-dashboard/flask/wrangling_scripts/wrangle_data.py
+++ python-data-dashboard/flask/wrangling_scripts/wrangle_data.py
@@ -11,9 +11,9 @@ def clean_data(data_path, keep_columns = ['country_name', '1990', '2015'],
     df.columns = df.columns.str.lower().str.replace(' ', '_')
     # Keep only the columns of interest (years and country name)
     df = df[keep_columns]
-
+    # Top 10 economies:
     countries_of_interest = ['United States', 'China', 'Japan', 'Germany',
-     'Denmark', 'India', 'France', 'Brazil', 'Russian Federation', 'Canada']
+    'United Kingdom', 'India', 'France', 'Brazil', 'Italy', 'Canada']
 
     df = df[df['country_name'].isin(countries_of_interest)]
 
@@ -21,6 +21,7 @@ def clean_data(data_path, keep_columns = ['country_name', '1990', '2015'],
     df_melt = df.melt(id_vars = 'country_name', value_vars = value_variables)
     df_melt.columns = ['country', 'year', 'variable']
     df_melt['year'] = pd.to_datetime(df_melt['year']).dt.year
+    return df_melt
 
 
 def return_figures():
@@ -33,6 +34,7 @@ def return_figures():
     # 1. plot arable land from 1990 to 2015 in top10 economies as line chart
     # ======================================================================
     graph_one = []
+
     df = clean_data('data/API_AG.LND.ARBL.HA.PC_DS2_en_csv_v2.csv')
     df.columns = ['country','year','hectares_arable_land_perperson']
     df.sort_values('hectares_arable_land_perperson',
@@ -44,7 +46,7 @@ def return_figures():
         xval = dfcountry['year'].tolist()
         yval = dfcountry['hectares_arable_land_perperson'].tolist()
 
-        graph_one.append(go.scatter(
+        graph_one.append(go.Scatter(
             x = xval, y = yval,
             mode = 'lines',
             name = country
@@ -53,7 +55,7 @@ def return_figures():
     layout_one = dict(
         title = 'Change in Hectares Arable Land <br> per Person 1990 to 2015',
         xaxis = dict(
-            title = 'yeaer',
+            title = 'year',
             autotick = False,
             tick0 = 1990,
             dtick = 25
@@ -61,4 +63,8 @@ def return_figures():
         yaxis = dict(title = 'Hectares')
     )
 
-    
+    # append all charts to the figures list
+    figures = []
+    figures.append(dict(data = graph_one, layout = layout_one))
+
+    return figures
