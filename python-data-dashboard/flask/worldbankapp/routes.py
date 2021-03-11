6fefb3805db3c208d50056e7dfa1b9cf4628d452
Heschmat <heschmat.seyedi@gmail.com>
2021-03-11 16:13:40 +0100

    Add a sample data and how to parse/wrangle data with Pandas.

--- python-data-dashboard/flask/worldbankapp/routes.py
+++ python-data-dashboard/flask/worldbankapp/routes.py
@@ -2,6 +2,14 @@ from worldbankapp import app
 
 from flask import render_template
 
+import pandas as pd
+from wrangling_scripts.wrangling import data_wrangling
+
+
+data = data_wrangling()
+# Test to see if wrangling works
+print(data)
+
 @app.route('/')
 @app.route('/index')
 def index():
