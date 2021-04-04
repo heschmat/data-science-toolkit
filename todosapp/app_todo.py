77cbe7849f0a20f941133f9317bb17ceb491d575
Heschmat <heschmat.seyedi@gmail.com>
2021-04-04 10:06:44 +0200

    Add get_list_todos(); Modify the code, homepage wont show all different tasks.

--- todosapp/app_todo.py
+++ todosapp/app_todo.py
@@ -2,11 +2,15 @@ import sys
 from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.sql.expression import false
 
 
 app = Flask(__name__)
 uri_db = 'postgres://postgres:huaamy@localhost:5432/todoapp'
 app.config['SQLALCHEMY_DATABASE_URI'] = uri_db
+# FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds
+# significant overhead and will be disabled by default in the future.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, directory= 'migrations')
 
@@ -20,7 +24,7 @@ class Todo(db.Model):
     list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable= False)
 
     def __repr__(self):
-        res = f'<Todo {self.id} {self.description}>'
+        res = f'<Todo {self.id}: {self.description} is {self.completed}>'
         return res
 
 
@@ -32,6 +36,9 @@ class TodoList(db.Model):
     # backref= 'list' ## custom name, referencing what the parent name should be.
     # lazy= True)
     todos = db.relationship('Todo', backref= 'list', lazy= True)
+
+    def __repr__(self):
+        return f'<TodoList name:{self.name}, todos: {self.todos}>'
 
 # # Make sure that the tables are created for all the models.
 # db.create_all()
@@ -68,12 +75,15 @@ def create_todo():
         # Get the JSON that comes back from the AJAX request.
         data = request.get_json()
         description = data['description']
+        list_id = data['list_id']
         
-        todo = Todo(description= description)
+        todo = Todo(description= description, list_id= list_id)
         db.session.add(todo)
         db.session.commit()
         # Add the info to the body.
+        body['id'] = todo.id
         body['description'] = todo.description
+        body['completed'] = todo.completed
 
     except:
         err = True
@@ -93,6 +103,7 @@ def create_todo():
 
 @app.route('/todos/<todo_id>/set-done', methods=['POST'])
 def set_task_done(todo_id):
+    err = False
     try:
         is_done = request.get_json()['completed']
         task = Todo.query.get(todo_id)
@@ -100,13 +111,20 @@ def set_task_done(todo_id):
         db.session.commit()
     except:
         db.session.rollback()
+        err = True
+        print(sys.exc_info())
     finally:
         db.session.close()
 
-    return redirect(url_for('index'))
+    if err:
+        abort(500)
+    else:
+        # Redirect to the function index()
+        return redirect(url_for('index'))
 
 @app.route('/todos/<todo_id>/delete-task', methods=['DELETE'])
 def remove_task(todo_id):
+    err = False
     try:
         task = Todo.query.get(todo_id)
         db.session.delete(task)
@@ -114,20 +132,30 @@ def remove_task(todo_id):
         db.session.commit()
     except:
         db.session.rollback()
+        err = True
     finally:
         db.session.close()
 
-    # return redirect(url_for('index'))
-    return jsonify({'success': True})
+    if err:
+        abort(500)
+    else:
+        return jsonify({'success': True})
 
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     # Make sure the only todos visible will be per list_id.
     # Order the tasks time-wise.
-    data = Todo.query.filter_by(list_id= list_id).order_by('id').all()
-    return render_template('index_todo.html', data= data)
+    lists = TodoList.query.all()
+    active_list = TodoList.query.get(list_id)
+    tasks = Todo.query.filter_by(list_id= list_id).order_by('id').all()
+    return render_template(
+        'index_todo.html',
+        lists= lists, active_list= active_list, tasks= tasks
+    )
 
 
 @app.route('/')
 def index():
+    # The homepage will be the tasks related to list_id = 1:
+    # i.e., `Uncategorized` tasks.
     return redirect(url_for('get_list_todos', list_id= 1))
